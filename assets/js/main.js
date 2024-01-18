b0317dfd1f9781e05b7b3f2640673b075cf61bee
Shagor Ahmed <mdsagorfreelancer@gmail.com>
2024-01-18 11:20:29 +0600

    first

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+// compare pricing table js
+$(document).ready(function(){
+    const toggleSwitch = document.querySelector(".toggle-switch");
+
+    toggleSwitch.addEventListener("change",()=>{
+        if(toggleSwitch.checked){
+            document.querySelector(".pricing-main").classList.add("active");
+        }
+        else{
+            document.querySelector(".pricing-main").classList.remove("active");
+        }
+    });
+});
